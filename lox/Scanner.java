6b6cbcb72245eb8a03e351a5a2c8a31b3b750783
samkelsey <samjkelsey1@gmail.com>
2023-07-19 21:35:52 +0100

    feat: Handle multi length lexemes in Scanner

--- lox/Scanner.java
+++ lox/Scanner.java
@@ -3,6 +3,8 @@ package lox;
 import java.util.ArrayList;
 import java.util.List;
 
+import static lox.TokenType.*;
+
 public class Scanner {
     private final String source;
     private final List<Token> tokens = new ArrayList<>();
@@ -33,9 +35,96 @@ public class Scanner {
         char c = advance();
         switch (c) {
             case '(': 
-                addToken(TokenType.LEFT_PAREN); 
+                addToken(LEFT_PAREN);
+                break;
+            case ')':
+                addToken(RIGHT_PAREN);
+                break;
+            case '{':
+                addToken(LEFT_BRACE);
+                break;
+            case '}':
+                addToken(RIGHT_BRACE);
+                break;
+            case ',':
+                addToken(COMMA);
+                break;
+            case '.':
+                addToken(DOT);
+                break;
+            case '-':
+                addToken(MINUS);
+                break;
+            case '+':
+                addToken(PLUS);
+                break;
+            case ';':
+                addToken(SEMICOLON);
+                break;
+            case '*':
+                addToken(STAR);
+                break;
+            case '!':
+                addToken(match('=') ? BANG_EQUAL : BANG);
+                break;
+            case '=':
+                addToken(match('=') ? EQUAL_EQUAL : EQUAL);
+                break;
+            case '<':
+                addToken(match('=') ? LESS_EQUAL : LESS);
+                break;
+            case '>':
+                addToken(match('=') ? GREATER_EQUAL : GREATER);
+                break;
+            case '/':
+                if (match('/')) {
+                    // A comment goes until the end of the line.
+                    while (peek() != '\n' && !isAtEnd()) {
+                        advance();
+                    }
+                } else {
+                    addToken(SLASH);
+                }
+
+                break;
+            case ' ':
+            case '\r':
+            case '\t':
+                // Ignore whitespace
+                break;
+            case '\n':
+                line++;
+                break;
+            default:
+                Lox.error(line, "Unexpected character.");
                 break;
         }
+    }
+
+    private char peek() {
+        if (isAtEnd()) {
+            return '\0';
+        }
+
+        return source.charAt(current);
+    }
+
+    /**
+     * Like a conditional advance, we only consume the current character if it's what we're looking for.
+     * @param expected
+     * @return
+     */
+    private boolean match(char expected) {
+        if (isAtEnd()) {
+            return false;
+        }
+
+        if (source.charAt(current) != expected) {
+            return false;
+        }
+
+        current++;
+        return true;
     }
 
     private void addToken(TokenType type) {
@@ -48,6 +137,9 @@ public class Scanner {
     }
 
     private char advance() {
-        return source.charAt(current++);
+        char c = source.charAt(current);
+        current++;
+
+        return c;
     }
 }
