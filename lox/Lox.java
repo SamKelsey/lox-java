20f19e10d9708e137a842a4d7472ed63eb0c1e2e
samkelsey <samjkelsey1@gmail.com>
2023-12-21 11:39:17 +0000

    finished wiring up parser with error handling

--- lox/Lox.java
+++ lox/Lox.java
@@ -49,11 +49,15 @@ public class Lox {
         Scanner scanner = new Scanner(source);
         List<Token> tokens = scanner.scanTokens();
 
-        // For now, just print the tokens
-        System.out.println("TOKEN TYPE  |  LEXEME  |  LITERAL");
-        for (Token token : tokens) {
-            System.out.println(token);
+        Parser parser = new Parser(tokens);
+        Expr expression = parser.parse();
+
+        // Stop if there was a syntax error
+        if (hadError) {
+            return;
         }
+
+        System.out.println(new AstPrinter().print(expression));
     }
 
     static void error(int line, String message) {
@@ -63,5 +67,13 @@ public class Lox {
     private static void report(int line, String where, String message) {
         System.out.println("[line " + line + "] Error" + where + ": " + message);
         hadError = true;
+    }
+
+    static void error(Token token, String message) {
+        if (token.type == TokenType.EOF) {
+            report(token.line, " at end", message);
+        } else {
+            report(token.line, " at  '" + token.lexeme + "'", message);
+        }
     }
 }
--- lox/Parser.java
+++ lox/Parser.java
@@ -5,11 +5,21 @@ import java.util.List;
 import static lox.TokenType.*;
 
 public class Parser {
+
+    private static class ParseError extends RuntimeException {}
     private final List<Token> tokens;
     private int current;
 
     Parser(List<Token> tokens) {
         this.tokens = tokens;
+    }
+
+    Expr parse() {
+        try {
+            return expression();
+        } catch (ParseError error) {
+            return null;
+        }
     }
 
     private Expr expression() {
@@ -93,6 +103,8 @@ public class Parser {
             consume(RIGHT_PAREN, "Expect ')' after expression.");
             return new Expr.Grouping(expr);
         }
+
+        throw error(peek(), "Expect expression.");
     }
 
     private boolean match(TokenType... types) {
@@ -104,6 +116,14 @@ public class Parser {
         }
 
         return false;
+    }
+
+    private Token consume(TokenType type, String message) {
+        if (check(type)) {
+            return advance();
+        }
+
+        throw error(peek(), message);
     }
 
     private boolean check(TokenType type) {
@@ -133,4 +153,29 @@ public class Parser {
     private Token previous() {
         return tokens.get(current - 1);
     }
+
+    private ParseError error(Token token, String message) {
+        Lox.error(token, message);
+        return new ParseError();
+    }
+
+    // We use this to discard tokens until the beginning of a statement.
+    // Useful for preventing cascading errors when parsing a statement.
+    // We get back to a valid token and continue.
+    private void synchronize() {
+        advance();
+        while (!isAtEnd()) {
+            if (previous().type == SEMICOLON) {
+                return;
+            }
+
+            switch (peek().type) {
+                case CLASS: case FOR: case FUN: case IF: case PRINT: case RETURN: case VAR: case WHILE:
+                    return;
+            }
+
+            advance();
+        }
+    }
+
 }
